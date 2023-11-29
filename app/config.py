08d62f0b15e8ddc5a650d502fdbb711ba182882b
infamous55 <infamouslegend555@gmail.com>
2023-11-29 19:27:05 +0200

    added basic auth

--- app/config.py
+++ app/config.py
@@ -2,13 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=(".env", "db.env"))
+    model_config = SettingsConfigDict(env_file=("app/.env", "db.env"))
 
     postgres_user: str
     postgres_password: str
     postgres_db: str
     postgres_host: str = "db"
     postgres_port: int = 5432
+    jwt_secret: str
 
 
 settings = Settings()  # pyright: ignore[reportGeneralTypeIssues]
--- app/models.py
+++ app/models.py
@@ -1,7 +1,19 @@
 from pydantic import BaseModel, EmailStr
 
 
+# TODO: validate the name
 class NewUser(BaseModel):
     name: str
     email: EmailStr
     password: str
+
+
+class Credentials(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
